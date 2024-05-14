9d6abe88b7547fe7cd6118d247fdcd45b3ede341
Ricardo Viana <richard@Ricardos-MacBook-Pro.local>
2024-05-14 13:57:52 -0300

    implement grpc

--- gateway/http_handler.go
+++ gateway/http_handler.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"errors"
 	"github.com/Ricardolv/commons"
 	pb "github.com/Ricardolv/commons/api"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	"net/http"
 )
 
@@ -28,8 +31,44 @@ func (h *handler) HandlerCretaeOrders(resp http.ResponseWriter, req *http.Reques
 		return
 	}
 
-	h.client.CreateOrder(req.Context(), &pb.CreateOderRequest{
+	if err := validateItems(items); err != nil {
+		commons.WriterError(resp, http.StatusBadRequest, err.Error())
+	}
+
+	order, err := h.client.CreateOrder(req.Context(), &pb.CreateOderRequest{
 		CustomerID: customerID,
 		Items:      items,
 	})
+	rStatus := status.Convert(err)
+
+	if rStatus != nil {
+		if rStatus.Code() != codes.InvalidArgument {
+			commons.WriterError(resp, http.StatusBadRequest, rStatus.Message())
+			return
+		}
+
+		commons.WriterError(resp, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	commons.WriteJSON(resp, http.StatusOK, order)
+}
+
+func validateItems(items []*pb.ItemsWithQuatity) error {
+
+	if len(items) == 0 {
+		return errors.New("items must have at least one item")
+	}
+
+	for _, item := range items {
+		if item.ID == "" {
+			return errors.New("item ID is required")
+		}
+
+		if item.Quantity <= 0 {
+			return errors.New("item must have a valid quantity")
+		}
+	}
+
+	return nil
 }
